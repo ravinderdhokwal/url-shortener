07e5876818b8aa4abc20c14fa255ad9b7db24b64
ravinderdhokwal <ravindercodes@gmail.com>
2026-08-10 20:33:00 +0530

    add url data model

---
+++ src/url_shortener/models/url_model.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timezone
+from typing import Optional
+from sqlalchemy import BigInteger, Boolean, DateTime, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+from url_shortener.db.base import Base
+
+
+class URLModel(Base):
+    __tablename__ = "urls"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True, autoincrement=True)
+    short_code: Mapped[str] = mapped_column(String(10), unique=True, index=True, nullable=False)
+    original_url: Mapped[str] = mapped_column(Text, nullable=False)
+
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    expires_at: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True), default=None)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now)
