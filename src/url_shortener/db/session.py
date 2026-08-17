0d93ee2efcd10d06bbaca578eeff42c227c33606
ravinderdhokwal <ravindercodes@gmail.com>
2026-08-17 18:54:37 +0530

    add session.commit() in get_db function to ensure changes are saved to the database

--- src/url_shortener/db/session.py
+++ src/url_shortener/db/session.py
@@ -18,6 +18,7 @@ async def get_db():
     async with AsyncSessionLocal() as session:
         try:
             yield session
+            await session.commit()
         except Exception:
             await session.rollback()
             raise
