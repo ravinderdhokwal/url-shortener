0661cfdd5aad86bec3d31d70c85a012831d15bb0
ravinderdhokwal <ravindercodes@gmail.com>
2026-08-17 18:14:15 +0530

    refactor router to import url_endpoints instead of urls for improved clarity

--- src/url_shortener/api/v1/router.py
+++ src/url_shortener/api/v1/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from url_shortener.api.v1.endpoints import urls
+from url_shortener.api.v1.endpoints import url_endpoints
 
 
 api_router_v1 = APIRouter()
 
-api_router_v1.include_router(urls.router)
+api_router_v1.include_router(url_endpoints.router)
