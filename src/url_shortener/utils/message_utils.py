0d9f823a44ee7eaafda0d0a2032d15de3d687f61
ravinderdhokwal <ravindercodes@gmail.com>
2026-08-19 13:10:09 +0530

    add messages for api responses

---
+++ src/url_shortener/utils/message_utils.py
@@ -0,0 +1,4 @@
+class Messages:
+    NO_SHORT_URL_IN_DB = "There are no short urls in the database."
+    SHORT_URL_ALREADY_EXISTS = "Short URL already exists for the entered url."
+    COULD_NOT_GENERATE_SHORT_URL = "Could not generate a short url, please try again later."
