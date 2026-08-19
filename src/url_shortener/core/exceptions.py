5806fa1a0ad88d6d0d1e5e23440d0a0220cc5ee0
ravinderdhokwal <ravindercodes@gmail.com>
2026-08-19 13:09:49 +0530

    add custom exception classes

--- src/url_shortener/core/exceptions.py
+++ src/url_shortener/core/exceptions.py
@@ -1,4 +1,21 @@
 class AppException(Exception):
-    def __init__(self, message: str, status_code: int = 400):
+    def __init__(self, message: str, status_code: int = 400, data: dict | list | None = None):
         self.message = message
         self.status_code = status_code
+        self.data = data
+
+class NotFoundError(AppException):
+    def __init__(self, message: str = "Resource not found"):
+        super().__init__(message, status_code=404)
+
+class UnauthorizedError(AppException):
+    def __init__(self, message: str = "Unauthorized"):
+        super().__init__(message, status_code=401)
+
+class ConflictError(AppException):
+    def __init__(self, message: str = "Resource already exists", data: dict | list | None = None):
+        super().__init__(message, status_code=409, data=data)
+
+class InternalServerError(AppException):
+    def __init__(self, message: str = "Internal Server Error"):
+        super().__init__(message, status_code=500)
